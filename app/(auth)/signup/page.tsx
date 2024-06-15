56cd030f6573c44376d4bbb8541914891cc858e3
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-15 16:50:10 +0530

    Dummy signup page

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,15 @@
+import LoginForm from "../components/LoginForm";
 
 const SignUpComponent = () => {
     return (
-        <div>SignUpComponent</div>
+        <div>
+            <h2 className="text-3xl font-bold my-5">
+                Welcome Back
+            </h2>
+            <div className="transition-all ease-linear h-full w-full max-w-96">
+                <LoginForm />
+            </div >
+        </div>
     )
 }
 
