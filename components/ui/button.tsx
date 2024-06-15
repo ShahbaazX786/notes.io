d4aa420782889668fc5d037c13bacf03ca08d482
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-15 17:36:13 +0530

    Added avatar, button, card from shadcn ui

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           "bg-zinc-100 text-zinc-900 shadow-sm hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
         ghost: "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
         link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
+        premium: "bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white border-0"
       },
       size: {
         default: "h-9 px-4 py-2",
