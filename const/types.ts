eccf6a773ff123519dae50da0af2d6f0b3b48b18
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-17 17:46:44 +0530

    Added types for constants

--- const/types.ts
+++ const/types.ts
@@ -6,7 +6,23 @@ export type LandingLayoutProps = {
   children: React.ReactNode;
 };
 
-
 export type HomeLayoutProps = {
   children: React.ReactNode;
 };
+
+export interface FooterMenuItem {
+  id: number;
+  link: string;
+  path: string;
+}
+
+export type FooterMenuTypes = FooterMenuItem[];
+
+export interface FooterSocialItem {
+  id: number;
+  name: string;
+  path: string;
+  icon: string | JSX.Element;
+}
+
+export type FooterSocialTypes = FooterSocialItem[];
