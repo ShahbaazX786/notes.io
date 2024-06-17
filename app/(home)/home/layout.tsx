c11b876c37e42b8b1874459294f043bb613df4af
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-17 16:41:51 +0530

    Added home layout

---
+++ app/(home)/home/layout.tsx
@@ -0,0 +1,21 @@
+import FooterComponent from "@/app/components/Footer";
+import NavbarComponent from "@/app/components/Navbar";
+import { HomeLayoutProps } from "@/const/types";
+
+const HomeLayout = ({ children }: HomeLayoutProps) => {
+    return (
+        <div>
+            <nav>
+                <NavbarComponent />
+            </nav>
+            <main>
+                {children}
+            </main>
+            <footer>
+                <FooterComponent />
+            </footer>
+        </div>
+    )
+}
+
+export default HomeLayout;
