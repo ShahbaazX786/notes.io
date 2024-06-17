181c2798d64ccec10ac15c3d3f3579baf258a52b
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-17 16:29:09 +0530

    Added signupForm schema

--- const/schema.ts
+++ const/schema.ts
@@ -8,3 +8,17 @@ export const LoginFormSchema = z.object({
     message: "Password must be at least 2 characters.",
   }),
 });
+
+export const SignupFormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z
+    .string()
+    .min(2, { message: "Email must be at least 2 characters." })
+    .email()
+    .max(50, { message: "Bruh!, you sure its a valid email!?" }),
+  password: z.string().min(2, {
+    message: "Password must be at least 2 characters.",
+  }),
+});
