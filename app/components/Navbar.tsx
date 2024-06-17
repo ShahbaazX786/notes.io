a794643aa56f3eb6daffee6fd5888afd3d0b771c
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-17 16:41:30 +0530

    Added navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+const NavbarComponent = () => {
+    return (
+        <div className="flex flex-row justify-between items-center">
+            <div>Brand Logo</div>
+            <div>Search Bar</div>
+            <div>Auth Status</div>
+        </div>
+    )
+}
+
+export default NavbarComponent;
