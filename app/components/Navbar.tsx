4fca27d8a484b9bdaa0b106c07254ddaa86fcff9
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-18 19:18:05 +0530

    Added Brand Logo with redirection.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
 
 const NavbarComponent = () => {
     return (
-        <div className="flex flex-row justify-between items-center">
-            <div>Brand Logo</div>
+        <div className="flex flex-row justify-between items-center px-4 py-2">
+            <Link href={'/home'} className="flex justify-center items-baseline">
+                <Image src={'/Logo.png'} alt="Brand Logo" height={24} width={24} quality={100} priority />
+                <p className="text-2xl font-bold">otes.io</p>
+            </Link>
             <div>Search Bar</div>
             <div>Auth Status</div>
         </div>
