6cd5a041e5238a3e84c0c471bb9d7a649119a509
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-18 20:32:37 +0530

    Updated stylings of notecard

--- app/(home)/home/components/NoteCard.tsx
+++ app/(home)/home/components/NoteCard.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import { CardBody, CardContainer, CardItem } from "@/components/3d_card";
@@ -6,8 +5,8 @@ import { NoteCardProps } from "@/const/types";
 
 const NoteCard = ({ note }: NoteCardProps) => {
     return (
-        <CardContainer className="inter-var">
-            <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardContainer className="inter-var flex flex-row justify-center items-center gap-5">
+            <CardBody className="bg-gray-50 relative group/card h-auto dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-8 border ">
                 <CardItem
                     translateZ="50"
                     className="text-xl font-bold text-neutral-600 dark:text-white"
