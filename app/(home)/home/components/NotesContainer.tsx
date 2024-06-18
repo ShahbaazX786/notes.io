6517bea861034921ef29eac6869c5d586a5465d6
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-18 21:23:18 +0530

    Added stylings for notes heading

--- app/(home)/home/components/NotesContainer.tsx
+++ app/(home)/home/components/NotesContainer.tsx
@@ -4,8 +4,8 @@ import NoteCard from "./NoteCard";
 
 const NotesContainer = () => {
     return (
-        <div className="container relative flex flex-col justify-center items-center">
-            <h2 className="my-5 text-4xl font-bold">Notes</h2>
+        <div className="container relative flex flex-col justify-center items-center my-8">
+            <h2 className="mb-4 text-4xl font-bold">Notes</h2>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 w-full">
                 {Notes.map((note) => (
                     <NoteCard key={note.id} note={note} />
