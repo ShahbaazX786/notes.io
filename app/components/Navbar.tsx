82387540ddcb7ea58ebee295965986bc47f5d3d6
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-18 21:23:35 +0530

    Added border and visiblity to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,10 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
-
 const NavbarComponent = () => {
     return (
-        <div className="flex flex-row justify-between items-center px-4 py-2">
+        <div className="flex flex-row justify-between items-center px-4 py-2 border-b-2 shadow-sm">
             <Link href={'/home'} className="flex justify-center items-baseline">
                 <Image src={'/Logo.png'} alt="Brand Logo" height={24} width={24} quality={100} priority />
                 <p className="text-2xl font-bold">otes.io</p>
