8f3c317f7614b09b68eb7db366ff831a9a1e5406
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-18 21:24:11 +0530

    Updated Create Note component.

--- app/(home)/home/components/CreateNote.tsx
+++ app/(home)/home/components/CreateNote.tsx
@@ -1,7 +1,31 @@
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { IoIosAddCircle } from "react-icons/io";
+
 
 const CreateNote = () => {
     return (
-        <div>CreateNote</div>
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant={"link"} size={"icon"} className="w-14 h-14 hover:rotate-90 hover:scale-110 transition-all ease-linear duration-300 rounded-full"><IoIosAddCircle size={48} /></Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Create New Note</DialogTitle>
+                    <DialogDescription>
+                        Add note form here
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+
     )
 }
 
--- app/(home)/home/components/NoteCard.tsx
+++ app/(home)/home/components/NoteCard.tsx
@@ -5,18 +5,18 @@ import { NoteCardProps } from "@/const/types";
 
 const NoteCard = ({ note }: NoteCardProps) => {
     return (
-        <CardContainer className="inter-var flex flex-row justify-center items-center my-4">
+        <CardContainer className="inter-var flex flex-row justify-center items-center my-4 drop-shadow-lg">
             <CardBody className="bg-gray-50 relative group/card h-auto dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-8 border ">
                 <CardItem
                     translateZ="50"
-                    className="text-xl font-bold text-neutral-600 dark:text-white"
+                    className="text-xl font-bold text-black dark:text-white"
                 >
                     {note.title}
                 </CardItem>
                 <CardItem
                     as="p"
                     translateZ="60"
-                    className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+                    className="text-gray-800 text-sm max-w-sm mt-2 dark:text-neutral-300"
                 >
                     {note.description}
                 </CardItem>
