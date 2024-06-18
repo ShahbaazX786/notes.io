fb0966abe7f2833ef0a18cab5f9e24de459934b5
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-18 19:18:16 +0530

    Updated home page.

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -1,8 +1,11 @@
-const HomeComponent = () => {
+import React from "react"
+
+const HomeComponent = ({ children }: { children: React.ReactNode }) => {
     return (
         <div>
+            {children}
         </div>
     )
 }
 
-export default HomeComponent
+export default HomeComponent;
