8499503807df80a898b6151371d46aa3d227f98c
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-19 12:15:06 +0530

    Added hover animation on options of notecard

--- app/(home)/home/components/NoteCardOptions.tsx
+++ app/(home)/home/components/NoteCardOptions.tsx
@@ -2,20 +2,20 @@ import CustomTooltip from "@/components/customShadcn/customTooltip";
 import { Button } from "@/components/ui/button";
 import { FaCheck } from "react-icons/fa";
 import { FiEdit } from "react-icons/fi";
-import { MdDeleteForever } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
+import { MdDeleteForever } from "react-icons/md";
 
 
 const NoteCardOptions = () => {
     return (
         <div className="flex flex-col justify-center items-center h-full w-full rounded-md from-black to-transparent bg-gradient-to-l px-4 gap-4">
             <div className="flex justify-between items-center gap-5">
-                <CustomTooltip icon={<Button variant={"destructive"}><MdDeleteForever /></Button>} name={'Delete Note'} />
-                <CustomTooltip icon={<Button variant={"premium"}><FiEdit /></Button>} name={'Edit Note'} />
+                <CustomTooltip icon={<Button variant={"destructive"} className="hover:scale-110 transition-all ease-linear" ><MdDeleteForever /></Button>} name={'Delete Note'} />
+                <CustomTooltip icon={<Button variant={"premium"} className="hover:scale-110 transition-all ease-linear" ><FiEdit /></Button>} name={'Edit Note'} />
             </div>
             <div className="flex justify-between items-center gap-5">
-                <CustomTooltip icon={<Button variant={"outline"}><FaCheck /></Button>} name={'Mark As Complete'} />
-                <CustomTooltip icon={<Button variant={"secondary"}><IoClose size={18}/></Button>} name={'Mark As Pending'} />
+                <CustomTooltip icon={<Button variant={"outline"} className="hover:scale-110 transition-all ease-linear" ><FaCheck /></Button>} name={'Mark As Complete'} />
+                <CustomTooltip icon={<Button variant={"secondary"} className="hover:scale-110 transition-all ease-linear" ><IoClose size={18} /></Button>} name={'Mark As Pending'} />
             </div>
         </div>
     )
