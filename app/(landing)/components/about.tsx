f2f79809dd36c362ccc99c39a8ebfffc491fa27a
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-19 12:49:57 +0530

    Changed stylings of about page.

--- app/(landing)/components/about.tsx
+++ app/(landing)/components/about.tsx
@@ -395,7 +395,7 @@ export function About() {
     ];
 
     return (
-        <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto bg-white dark:bg-black relative w-full">
+        <div className="flex flex-row items-center justify-center py-20 h-screen md:h-auto bg-[#111827] dark:bg-black relative w-full">
             <div className="max-w-7xl mx-auto w-full relative overflow-hidden h-full md:h-[40rem] px-4">
                 <motion.div
                     initial={{
@@ -411,16 +411,16 @@ export function About() {
                     }}
                     className="div"
                 >
-                    <h2 className="text-center text-xl md:text-4xl font-bold text-black dark:text-white">
+                    <h2 className="text-center text-xl md:text-4xl font-bold text-gray-200 dark:text-white">
                         Add notes from anywhere around the world
                     </h2>
-                    <p className="text-center text-base md:text-lg font-normal text-neutral-700 dark:text-neutral-200 max-w-md mt-2 mx-auto">
+                    <p className="text-center text-base md:text-lg font-normal text-neutral-400 dark:text-neutral-200 max-w-md mt-2 mx-auto">
                         This globe is interactive and customizable. Have fun with it, and
                         don&apos;t forget to share it. :)
                     </p>
                 </motion.div>
-                <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black to-white z-40" />
-                <div className="absolute w-full -bottom-20 h-72 md:h-full z-10">
+                <div className="absolute w-full bottom-0 inset-x-0 h-40 bg-gradient-to-b pointer-events-none select-none from-transparent dark:to-black rounded-lg z-40" />
+                <div className="absolute w-full -bottom-20 h-72 md:h-full z-10 my-8">
                     <World data={sampleArcs} globeConfig={globeConfig} />;
                 </div>
             </div>
