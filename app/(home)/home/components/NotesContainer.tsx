43f1606bafdd05c3fcd281039e5546a2e0298153
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-19 12:00:47 +0530

    Made create button fixed

--- app/(home)/home/components/NotesContainer.tsx
+++ app/(home)/home/components/NotesContainer.tsx
@@ -4,14 +4,14 @@ import NoteCard from "./NoteCard";
 
 const NotesContainer = () => {
     return (
-        <div className="container relative flex flex-col justify-center items-center my-8">
+        <div className="container flex flex-col justify-center items-center my-8">
             <h2 className="mb-4 text-4xl font-bold">Notes</h2>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 w-full">
                 {Notes.map((note) => (
                     <NoteCard key={note.id} note={note} />
                 ))}
             </div>
-            <div className="absolute bottom-5 right-5">
+            <div className="fixed bottom-8 right-8">
                 <CreateNote />
             </div>
         </div>
