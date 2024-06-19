2284421df6cc2c5e31ad74bf41c8aea95b4ae6bc
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-06-19 12:18:34 +0530

    Added delay in tooltip

--- app/(home)/home/components/NoteCardOptions.tsx
+++ app/(home)/home/components/NoteCardOptions.tsx
@@ -10,12 +10,12 @@ const NoteCardOptions = () => {
     return (
         <div className="flex flex-col justify-center items-center h-full w-full rounded-md from-black to-transparent bg-gradient-to-l px-4 gap-4">
             <div className="flex justify-between items-center gap-5">
-                <CustomTooltip icon={<Button variant={"destructive"} className="hover:scale-110 transition-all ease-linear" ><MdDeleteForever /></Button>} name={'Delete Note'} />
-                <CustomTooltip icon={<Button variant={"premium"} className="hover:scale-110 transition-all ease-linear" ><FiEdit /></Button>} name={'Edit Note'} />
+                <CustomTooltip delay={400} icon={<Button variant={"destructive"} className="hover:scale-110 transition-all ease-linear" ><MdDeleteForever /></Button>} name={'Delete Note'} />
+                <CustomTooltip delay={400} icon={<Button variant={"premium"} className="hover:scale-110 transition-all ease-linear" ><FiEdit /></Button>} name={'Edit Note'} />
             </div>
             <div className="flex justify-between items-center gap-5">
-                <CustomTooltip icon={<Button variant={"outline"} className="hover:scale-110 transition-all ease-linear" ><FaCheck /></Button>} name={'Mark As Complete'} />
-                <CustomTooltip icon={<Button variant={"secondary"} className="hover:scale-110 transition-all ease-linear" ><IoClose size={18} /></Button>} name={'Mark As Pending'} />
+                <CustomTooltip delay={400} icon={<Button variant={"outline"} className="hover:scale-110 transition-all ease-linear" ><FaCheck /></Button>} name={'Mark As Complete'} />
+                <CustomTooltip delay={400} icon={<Button variant={"secondary"} className="hover:scale-110 transition-all ease-linear" ><IoClose size={18} /></Button>} name={'Mark As Pending'} />
             </div>
         </div>
     )
