31f5dbe50fdba90975fc056eed6a016e92108535
shahbaazx786 <shahbaazplatinum@gmail.com>
2024-07-04 18:14:38 +0530

    Added fetchNotes api

--- app/(home)/home/components/NotesContainer.tsx
+++ app/(home)/home/components/NotesContainer.tsx
@@ -1,11 +1,26 @@
 'use client';
 
-import { Notes } from "@/lib/const/constants";
 import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 import CreateNote from "./CreateNote";
 import NoteCard from "./NoteCard";
 
 const NotesContainer = () => {
+
+    const [notes, setNotes] = useState([]);
+    useEffect(() => {
+        fetchAllNotes();
+    }, [])
+
+    const fetchAllNotes = async () => {
+        const res = await fetch('http://localhost:3000/notes');
+        const Notes = await res.json();
+        if (Notes.success) {
+            setNotes(Notes.data);
+        }
+        console.log(Notes);
+    }
+
     return (
         <div className="container flex flex-col justify-center items-center my-8">
             <motion.h2
@@ -24,22 +39,30 @@ const NotesContainer = () => {
             >Notes
             </motion.h2>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 w-full">
-                {Notes.map((note) => (
-                    <motion.div key={note.id} className="box" initial={{ opacity: 0, scale: 0.5 }}
-                        animate={{ opacity: 1, scale: 1 }}
-                        transition={{
-                            duration: 0.5,
-                            ease: [0, 0.71, 0.2, 1.01],
-                            scale: {
-                                type: "spring",
-                                damping: 5,
-                                stiffness: 100,
-                                restDelta: 0.001
-                            }
-                        }}>
-                        <NoteCard note={note} />
-                    </motion.div>
-                ))}
+                {notes && notes.length > 0 ? (
+                    notes.map((note) => (
+                        <motion.div
+                            key={note._id}
+                            className="box"
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{
+                                duration: 0.5,
+                                ease: [0, 0.71, 0.2, 1.01],
+                                scale: {
+                                    type: "spring",
+                                    damping: 5,
+                                    stiffness: 100,
+                                    restDelta: 0.001
+                                }
+                            }}
+                        >
+                            <NoteCard note={note} />
+                        </motion.div>
+                    ))
+                ) : (
+                    <h3 className="text-center">No Notes Found!</h3>
+                )}
             </div>
             <div className="fixed bottom-8 right-8">
                 <CreateNote />
