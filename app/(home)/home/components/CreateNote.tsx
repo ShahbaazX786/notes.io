453ada29c6b1d7597a211fa124fdd030d7404066
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-04-25 20:29:08 +0530

    Increased button size.

--- app/(home)/home/components/CreateNote.tsx
+++ app/(home)/home/components/CreateNote.tsx
@@ -1,32 +1,34 @@
 import { Button } from "@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { IoIosAddCircle } from "react-icons/io";
 
-
 const CreateNote = () => {
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button variant={"link"} size={"icon"} className="w-14 h-14 hover:rotate-90 hover:scale-110 transition-all ease-linear duration-300 rounded-full"><IoIosAddCircle size={52} /></Button>
-            </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Create New Note</DialogTitle>
-                    <DialogDescription>
-                        Add note form here
-                    </DialogDescription>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>
-
-    )
-}
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant={"link"}
+          size={"icon"}
+          className="w-20 h-20 hover:rotate-180 hover:scale-110 transition-all ease-in-out duration-300 rounded-full"
+        >
+          <IoIosAddCircle size={75} />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create New Note</DialogTitle>
+          <DialogDescription>Add note form here</DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default CreateNote;
