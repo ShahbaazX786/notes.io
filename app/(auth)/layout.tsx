7da40da8b6ac4e26ad91d28ee171696e78605a12
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-05 12:04:27 +0530

    Layout styling improvements.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactChildren } from "@/lib/const/types";
 
 const AuthLayout = ({ children }: ReactChildren) => {
   return (
-    <div className="w-full h-full flex justify-center items-center bg-gray-300">
+    <div className="w-full h-full flex justify-center items-center bg-gray-300 overflow-hidden">
       {children}
     </div>
   );
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import AuthForm from "../components/AuthForm";
 
 const LoginPage = () => {
   return (
-    <div>
+    <div className="w-full h-screen overflow-y-auto flex flex-col justify-normal md:justify-center items-center">
       <AuthForm mode="login" />
     </div>
   );
