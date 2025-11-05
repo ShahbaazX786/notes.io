84ed5e1de7c5ed06e87502d6c7fede5118b7cefe
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-05 11:33:41 +0530

    Simplified login form.

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,16 +1,11 @@
-import LoginForm from "../components/LoginForm";
+import AuthForm from "../components/AuthForm";
 
 const LoginPage = () => {
-    return (
-        <div>
-            <h2 className="text-3xl font-bold my-5">
-                Welcome Back
-            </h2>
-            <div className="transition-all ease-linear h-full w-full max-w-96">
-                <LoginForm />
-            </div >
-        </div>
-    );
-}
+  return (
+    <div>
+      <AuthForm mode="login" />
+    </div>
+  );
+};
 
 export default LoginPage;
