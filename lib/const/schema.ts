034620ccd430ca6223d5c85ccde6e9ee5802487a
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-05 11:37:31 +0530

    updated schema.

--- lib/const/schema.ts
+++ lib/const/schema.ts
@@ -10,10 +10,18 @@ export const LoginFormSchema = z.object({
 });
 
 export const SignupFormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  firstName: z.string().min(2, {
+    message: "First name must be at least 2 characters.",
+  }),
+  lastName: z.string().min(2, {
+    message: "Last name must be at least 2 characters.",
   }),
   email: z
+    .string()
+    .min(2, { message: "Email must be at least 2 characters." })
+    .email()
+    .max(50, { message: "Bruh!, you sure its a valid email!?" }),
+  confirmEmail: z
     .string()
     .min(2, { message: "Email must be at least 2 characters." })
     .email()
