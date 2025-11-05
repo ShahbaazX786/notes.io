aac0658b82db038746e598b392d0a6d010d04b9b
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-05 11:33:15 +0530

    Simplified signup form

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,16 +1,11 @@
-import SignupForm from "../components/SignupForm";
+import AuthForm from "../components/AuthForm";
 
 const SignUpComponent = () => {
-    return (
-        <div>
-            <h2 className="text-3xl font-bold my-5">
-                Welcome Back
-            </h2>
-            <div className="transition-all ease-linear h-full w-full max-w-96">
-                <SignupForm />
-            </div >
-        </div>
-    )
-}
+  return (
+    <div>
+      <AuthForm mode="signup" />
+    </div>
+  );
+};
 
 export default SignUpComponent;
