cef3f2ab9b353c3480f9364d0f78433b9cead352
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-06 12:07:34 +0530

    Updated add note comp.

--- app/(home)/home/components/AddNoteForm.tsx
+++ app/(home)/home/components/AddNoteForm.tsx
@@ -2,15 +2,53 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/ui/use-toast";
+import useNotes from "@/lib/hooks/useNotes";
 import { useState } from "react";
 import TagInput from "./TagInput";
+import { FunctionProp } from "@/lib/const/types";
 
-const AddNoteForm = () => {
+const AddNoteForm = ({ toggle }: FunctionProp) => {
+  const { createNoteMutation } = useNotes();
+
   const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+  const [description, setDescription] = useState("");
   const [tags, setTags] = useState([]);
 
-  const handleNote = () => {};
+  const handleNote = async () => {
+    if (!title.trim() || !description.trim()) {
+      toast({
+        title: "Error",
+        description: "Atleast Enter the Title to create a Note.",
+        variant: "destructive",
+      });
+    }
+
+    const noteData = { title, description, tags };
+
+    createNoteMutation.mutate(noteData, {
+      onSuccess: () => {
+        setTitle("");
+        setDescription("");
+        setTags([]);
+        toggle(false);
+        toast({
+          title: "Note added successfully!",
+          variant: "default",
+        });
+      },
+      onError: (err: any) => {
+        console.error("Error creating note:", err);
+        toast({
+          title: `${err?.response?.data?.error}`,
+          description: Array.isArray(err?.response?.data?.message)
+            ? err?.response?.data?.message.join(", ")
+            : err?.message,
+          variant: "destructive",
+        });
+      },
+    });
+  };
 
   return (
     <div className="flex flex-col justify-start items-center gap-4">
@@ -37,8 +75,8 @@ const AddNoteForm = () => {
           placeholder="Enter any Valid Message"
           id="NoteContent"
           rows={10}
-          value={content}
-          onChange={({ target }) => setContent(target.value)}
+          value={description}
+          onChange={({ target }) => setDescription(target.value)}
         />
       </div>
       <div className="flex flex-col w-full justify-center items-start space-x-2">
