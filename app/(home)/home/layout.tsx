10fe0d86f45b5310c0a131f4f3cb451f62c46a7c
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-06 09:32:22 +0530

    Updated layout.

--- app/(home)/home/layout.tsx
+++ app/(home)/home/layout.tsx
@@ -1,15 +1,10 @@
-import FooterComponent from "@/components/custom/Footer";
-import NavbarComponent from "@/components/custom/Navbar";
 import { ReactChildren } from "@/lib/const/types";
+import SideBar from "./components/sideBar";
 
 const HomeLayout = ({ children }: ReactChildren) => {
   return (
-    <div className="grid grid-rows-[auto_80%_auto] w-full h-screen">
-      <NavbarComponent />
-      <main className="flex flex-col justify-center items-center">
-        {children}
-      </main>
-      <FooterComponent />
+    <div className="flex flex-col justify-center items-center w-full h-full">
+      <SideBar>{children}</SideBar>
     </div>
   );
 };
