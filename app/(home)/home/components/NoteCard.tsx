e118424930c8beaa1d24c181a2b5331bbbcc545b
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-06 11:09:46 +0530

    minor style improvements.

--- app/(home)/home/components/NoteCard.tsx
+++ app/(home)/home/components/NoteCard.tsx
@@ -13,36 +13,36 @@ import { formatDate, formatTime } from "@/lib/utils";
 
 const NoteCard = ({ note }: NoteCardProps | any) => {
   return (
-    <CardContainer className="inter-var flex flex-row justify-center items-center drop-shadow-lg relative group">
-      <CardBody className="bg-gray-50 relative group/card h-auto dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-8 border ">
+    <CardContainer className="inter-var flex flex-row justify-center items-center drop-shadow-lg relative group w-full h-full">
+      <CardBody className="bg-gray-50 relative group/card h-auto dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] rounded-xl p-8 border max-h-40">
         <CardItem
           translateZ="50"
           className="absolute text-sm text-gray dark:text-white right-5 top-5"
         >
           <div className="flex justify-center items-center gap-2 ">
             <SlCalender />{" "}
-            <p className="text-sm">
+            <p className="text-sm line-clamp-1 ">
               {formatDate(note.createdAt) + " | " + formatTime(note.createdAt)}
             </p>
           </div>
         </CardItem>
         <CardItem
           translateZ="50"
-          className="text-xl font-bold text-black dark:text-white first-letter:capitalize"
+          className="text-xl font-bold text-black dark:text-white first-letter:capitalize line-clamp-1 max-w-72 text-ellipsis mt-2"
         >
           {note.title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
-          className="text-gray-800 text-sm max-w-sm mt-2 dark:text-neutral-300 first-letter:capitalize"
+          className="text-gray-800 text-sm max-w-xs mt-2 dark:text-neutral-300 first-letter:capitalize line-clamp-2"
         >
           {note.description}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
-          className="text-gray-800 text-sm max-w-sm mt-2 dark:text-neutral-300 first-letter:capitalize"
+          className="text-gray-800 text-sm max-w-sm mt-2 dark:text-neutral-300 first-letter:capitalize line-clamp-1"
         >
           {"Tags: " + note.tags}
         </CardItem>
