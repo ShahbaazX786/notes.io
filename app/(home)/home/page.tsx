439beb3bba8ec0f399d75a624c1a752e47da315f
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-06 09:56:50 +0530

    Updated home page.

--- app/(home)/home/page.tsx
+++ app/(home)/home/page.tsx
@@ -1,6 +1,9 @@
+import HomeHeader from "./components/homeHeader";
+
 const HomeComponent = () => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full flex justify-start items-start flex-col">
+      <HomeHeader />
       <h1 className="text-2xl font-semibold text-neutral-800 dark:text-neutral-100">
         Dashboard
       </h1>
