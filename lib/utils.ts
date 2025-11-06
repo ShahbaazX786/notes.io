4f73d6b61f3cd12167396ab3f5fa03c64ee418e2
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-06 10:52:53 +0530

    added util methods.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,29 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+const formatDate = (isoString: string | Date) => {
+  const date = new Date(isoString);
+
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+
+  return `${day}-${month}-${year}`;
+};
+
+const formatTime = (timeString: string | Date) => {
+  const date = new Date(timeString);
+  return date
+    .toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    })
+    .toLowerCase();
+};
+
+export { formatDate, formatTime };
