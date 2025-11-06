0bec2ee69797fe6229a7d362c54fba5ecabcf963
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-06 09:31:25 +0530

    updated nextconfig temporarily.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "assets.aceternity.com",
+                port: "",
+                pathname: "/**", // allow all image paths from this host
+            }
+        ]
+    }
+};
 
 export default nextConfig;
