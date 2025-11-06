d9d46db64699594049826b384c0eb00d3a19ed4b
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-06 13:03:10 +0530

    updated notes comp.

--- app/(home)/home/components/NoteCardOptions.tsx
+++ app/(home)/home/components/NoteCardOptions.tsx
@@ -1,49 +1,112 @@
 import CustomTooltip from "@/components/customShadcn/customTooltip";
 import { Button } from "@/components/ui/button";
+import { toast } from "@/components/ui/use-toast";
+import useNotes from "@/lib/hooks/useNotes";
 import { FaCheck } from "react-icons/fa";
 import { FiEdit } from "react-icons/fi";
 import { IoClose } from "react-icons/io5";
 import { MdDeleteForever } from "react-icons/md";
 
+const NoteCardOptions = ({ uniqueId }: any) => {
+  const { updateNoteMutation, deleteNoteMutation } = useNotes();
+  const handleDelete = () => {
+    deleteNoteMutation.mutate(uniqueId, {
+      onSuccess: () => {
+        toast({
+          title: "Note deleted successfully!",
+          variant: "default",
+        });
+      },
+      onError: (err: any) => {
+        toast({
+          title: "Error deleting note",
+          description:
+            err?.response?.data?.message ||
+            err?.message ||
+            "Something went wrong",
+          variant: "destructive",
+        });
+      },
+    });
+  };
 
-const NoteCardOptions = ({uniqueId}:any) => {
-    console.log('heheeh',uniqueId)
-    const deleteNote = async () => {
-        const url = `http://localhost:3000/notes/${uniqueId}`;
-        console.log(url);
+  const handleUpdate = () => {
+    updateNoteMutation.mutate(uniqueId, {
+      onSuccess: () => {
+        toast({
+          title: "Note marked as complete ✅",
+          variant: "default",
+        });
+      },
+      onError: () => {
+        toast({
+          title: "Failed to mark note as complete ❌",
+          variant: "destructive",
+        });
+      },
+    });
+  };
 
-        try {
-            const response = await fetch(url, {
-                method: 'DELETE',
-                headers: {
-                    'Content-Type': 'application/json',
-                },  
-            });
-
-            if (!response.ok) {
-                throw new Error('Network response was not ok' + response.statusText);
-            }
-
-            const data = await response.json();
-            console.log('Delete successful:', data);
-            return data;
-        } catch (error) {
-            console.error('There was a problem with the fetch operation:', error);
-        }
-    };
-
-    return (
-        <div className="flex flex-col justify-center items-center h-full w-full rounded-md from-black to-transparent bg-gradient-to-l px-4 gap-4">
-            <div className="flex justify-between items-center gap-5">
-                <CustomTooltip delay={400} icon={<Button onClick={deleteNote} variant={"destructive"} className="hover:scale-110 transition-all ease-linear" ><MdDeleteForever /></Button>} name={'Delete Note'} />
-                <CustomTooltip delay={400} icon={<Button variant={"premium"} className="hover:scale-110 transition-all ease-linear" ><FiEdit /></Button>} name={'Edit Note'} />
-            </div>
-            <div className="flex justify-between items-center gap-5">
-                <CustomTooltip delay={400} icon={<Button variant={"outline"} className="hover:scale-110 transition-all ease-linear" ><FaCheck /></Button>} name={'Mark As Complete'} />
-                <CustomTooltip delay={400} icon={<Button variant={"secondary"} className="hover:scale-110 transition-all ease-linear" ><IoClose size={18} /></Button>} name={'Mark As Pending'} />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full rounded-md from-black to-transparent bg-gradient-to-l px-4 gap-4">
+      <div className="flex justify-between items-center gap-5">
+        <CustomTooltip
+          delay={400}
+          icon={
+            <Button
+              onClick={handleDelete}
+              variant={"destructive"}
+              className="hover:scale-110 transition-all ease-linear"
+            >
+              <MdDeleteForever />
+            </Button>
+          }
+          name={"Delete Note"}
+        />
+        <CustomTooltip
+          delay={400}
+          icon={
+            <Button
+              onClick={handleUpdate}
+              variant={"premium"}
+              className="hover:scale-110 transition-all ease-linear"
+            >
+              <FiEdit />
+            </Button>
+          }
+          name={"Edit Note"}
+        />
+      </div>
+      <div className="flex justify-between items-center gap-5">
+        <CustomTooltip
+          delay={400}
+          icon={
+            <Button
+              onClick={handleUpdate}
+              variant={"outline"}
+              className="hover:scale-110 transition-all ease-linear"
+            >
+              <FaCheck />
+            </Button>
+          }
+          name={"Mark As Complete"}
+        />
+        <CustomTooltip
+          delay={400}
+          icon={
+            <Button
+              onClick={handleUpdate}
+              variant={"secondary"}
+              className="hover:scale-110 transition-all ease-linear"
+            >
+              <IoClose size={18} />
+            </Button>
+          }
+          name={"Mark As Pending"}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default NoteCardOptions;
