96f3de5c38d4b51373c6ecfeef990af98e916935
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-07 10:59:39 +0530

    Utilized store variables to manage the component.

--- app/(home)/home/components/AddNoteForm.tsx
+++ app/(home)/home/components/AddNoteForm.tsx
@@ -2,13 +2,15 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { toast } from "@/components/ui/use-toast";
+import { badToast, goodToast } from "@/lib/api/api-status";
 import useNotes from "@/lib/hooks/useNotes";
-import { useState } from "react";
+import { useNoteStore } from "@/lib/store/noteStore";
+import { useEffect, useState } from "react";
 import TagInput from "./TagInput";
 
 const AddNoteForm = () => {
   const { createNoteMutation } = useNotes();
+  const { editableNote, setIsModalOpen } = useNoteStore();
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -16,11 +18,7 @@ const AddNoteForm = () => {
 
   const handleNote = async () => {
     if (!title.trim() || !description.trim()) {
-      toast({
-        title: "Error",
-        description: "Atleast Enter the Title to create a Note.",
-        variant: "destructive",
-      });
+      badToast("Error", "Atleast Enter the Title to create a Note.");
     }
 
     const noteData = { title, description, tags };
@@ -30,36 +28,32 @@ const AddNoteForm = () => {
         setTitle("");
         setDescription("");
         setTags([]);
-        // toggle(false);
-        toast({
-          title: "Note added successfully!",
-          variant: "default",
-        });
+        setIsModalOpen(false);
+        goodToast("Note added sucessfully");
       },
       onError: (err: any) => {
         console.error("Error creating note:", err);
-        toast({
-          title: `${err?.response?.data?.error}`,
-          description: Array.isArray(err?.response?.data?.message)
+        badToast(
+          err?.response?.data?.error,
+          Array.isArray(err?.response?.data?.message)
             ? err?.response?.data?.message.join(", ")
-            : err?.message,
-          variant: "destructive",
-        });
+            : err?.message
+        );
       },
     });
   };
 
-  // useEffect(() => {
-  //   if (note) {
-  //     setTitle(note.title);
-  //     setDescription(note.description);
-  //     setTags(note.tags || []);
-  //   } else {
-  //     setTitle("");
-  //     setDescription("");
-  //     setTags([]);
-  //   }
-  // }, [note]);
+  useEffect(() => {
+    if (editableNote) {
+      setTitle(editableNote?.title);
+      setDescription(editableNote?.description);
+      setTags(editableNote?.tags || []);
+    } else {
+      setTitle("");
+      setDescription("");
+      setTags([]);
+    }
+  }, [editableNote]);
 
   return (
     <div className="flex flex-col justify-start items-center gap-4">
@@ -94,7 +88,7 @@ const AddNoteForm = () => {
         <TagInput tags={tags} setTags={setTags} />
       </div>
       <Button type="submit" className="w-full" onClick={handleNote}>
-        {true ? "Update Note" : "Add New Note"}
+        {editableNote ? "Update Note" : "Add New Note"}
       </Button>
     </div>
   );
