b066ac05086012354cfa9f21d8bc4181272d2850
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-07 10:44:50 +0530

    Added new notestore.

---
+++ lib/store/noteStore.ts
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+import { NoteStore } from "../const/types";
+
+const useNoteStore = create<NoteStore>((set) => ({
+  editableNote: null,
+  setEditableNote: (note) => set({ editableNote: note }),
+
+  isModalOpen: false,
+  setIsModalOpen: (state) => set({ isModalOpen: state }),
+}));
+
+export { useNoteStore };
