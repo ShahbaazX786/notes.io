af194ae40033488fa704e84a34763778c2dd52b4
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 17:52:06 +0530

    added variants for create note button.

--- app/(dashboard)/components/CreateNote.tsx
+++ app/(dashboard)/components/CreateNote.tsx
@@ -9,8 +9,10 @@ import {
 import { IoIosAddCircle } from "react-icons/io";
 import AddNoteForm from "./AddNoteForm";
 import { useNoteStore } from "@/lib/store/noteStore";
+import { usePathname } from "next/navigation";
 
-const CreateNote = () => {
+const CreateNote = ({ variant }: { variant: string }) => {
+  const currentPath = usePathname();
   const { isModalOpen, setIsModalOpen, editableNote, setEditableNote } =
     useNoteStore();
 
@@ -21,15 +23,24 @@ const CreateNote = () => {
 
   return (
     <Dialog open={isModalOpen} onOpenChange={(open) => handleOpenChanges(open)}>
-      <DialogTrigger asChild>
-        <Button
-          variant={"link"}
-          size={"icon"}
-          className="w-14 h-14 md:w-20 md:h-20 hover:rotate-180 hover:scale-110 transition-all ease-in-out duration-300 rounded-full"
-          onClick={() => setIsModalOpen(true)}
-        >
-          <IoIosAddCircle size={52} />
-        </Button>
+      <DialogTrigger asChild hidden={currentPath !== "/notes"}>
+        {variant === "default" ? (
+          <Button
+            variant={"link"}
+            size={"icon"}
+            className="w-14 h-14 md:w-20 md:h-20 hover:rotate-180 hover:scale-110 transition-all ease-in-out duration-300 rounded-full"
+            onClick={() => setIsModalOpen(true)}
+          >
+            <IoIosAddCircle size={52} />
+          </Button>
+        ) : (
+          <button tabIndex={0} className="p-[3px] relative">
+            <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+            <div className="px-8 py-1  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+              New Note
+            </div>
+          </button>
+        )}
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
