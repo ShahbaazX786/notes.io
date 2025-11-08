9570f931fb9ffa06c2ec1a54cb0b5d6d360873a8
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 16:57:07 +0530

    updated archive page.

--- app/(dashboard)/(routes)/archive/page.tsx
+++ app/(dashboard)/(routes)/archive/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
+import { Note } from "@/lib/const/types";
+import { useNoteStore } from "@/lib/store/noteStore";
+import NoteCard from "../../components/NoteCard";
 
 const ArchivePage = () => {
-  return <div>ArchivePage</div>;
+  const { notes } = useNoteStore();
+
+  return (
+    <div className="flex justify-center items-start flex-wrap gap-6 w-[99%] mx-auto overflow-y-auto pb-10 pt-8">
+      {notes.length > 0 &&
+        notes.map((note: Note) => <NoteCard key={note._id} note={note} />)}
+    </div>
+  );
 };
 
 export default ArchivePage;
