7b2337543191975b897c269410ba5b872a47ed35
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 13:32:10 +0530

    Changed dashboard layout.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,19 @@
 import { ReactChildren } from "@/lib/const/types";
-import SideBar from "./components/sideBar";
-
+import {
+  Sidebar,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
 const HomeLayout = ({ children }: ReactChildren) => {
   return (
-    <div className="flex flex-col justify-center items-center w-full h-full">
-      <SideBar>{children}</SideBar>
+    <div className="flex flex-col justify-center items-center w-full h-full relative">
+      <SidebarProvider>
+        <Sidebar />
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
     </div>
   );
 };
