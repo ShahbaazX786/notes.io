98ce550673373e0bfdee2ae3a88a807ed1d36af9
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 17:51:47 +0530

    removed create note from absolute bottom.

--- app/(dashboard)/(routes)/notes/page.tsx
+++ app/(dashboard)/(routes)/notes/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Note } from "@/lib/const/types";
-import CreateNote from "../../components/CreateNote";
 import NoteCard from "../../components/NoteCard";
 import NoNotes from "../../components/NoNotes";
 import { useNoteStore } from "@/lib/store/noteStore";
@@ -10,9 +9,6 @@ const NotesPage = () => {
   const { notes } = useNoteStore();
   return (
     <div className="w-full h-full flex justify-start items-start flex-col relative">
-      <div className="absolute bottom-0 right-0">
-        <CreateNote />
-      </div>
       <div className="flex justify-center items-start flex-wrap gap-6 w-[99%] mx-auto overflow-y-auto pb-10 pt-8">
         {notes.length > 0 ? (
           notes.map((note: Note) => <NoteCard key={note._id} note={note} />)
