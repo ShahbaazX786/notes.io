024a09bc7948f2190027446eef79b5eb7306e256
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 16:37:46 +0530

    moved the fetch and state update logic to dashboard layout.

--- app/(dashboard)/(routes)/notes/page.tsx
+++ app/(dashboard)/(routes)/notes/page.tsx
@@ -1,27 +1,21 @@
 "use client";
 
-import useNotes from "@/lib/hooks/useNotes";
 import { Note } from "@/lib/const/types";
 import CreateNote from "../../components/CreateNote";
 import NoteCard from "../../components/NoteCard";
 import NoNotes from "../../components/NoNotes";
+import { useNoteStore } from "@/lib/store/noteStore";
 
 const NotesPage = () => {
-  const { notesQuery } = useNotes();
-
-  if (notesQuery.isLoading) return <p>Loading...</p>;
-  if (notesQuery.isError) return <p>Error fetching notes.</p>;
-
+  const { notes } = useNoteStore();
   return (
     <div className="w-full h-full flex justify-start items-start flex-col relative">
       <div className="absolute bottom-0 right-0">
         <CreateNote />
       </div>
       <div className="flex justify-center items-start flex-wrap gap-6 w-[99%] mx-auto overflow-y-auto pb-10 pt-8">
-        {notesQuery.data?.data.length > 0 ? (
-          notesQuery.data?.data?.map((note: Note) => (
-            <NoteCard key={note._id} note={note} />
-          ))
+        {notes.length > 0 ? (
+          notes.map((note: Note) => <NoteCard key={note._id} note={note} />)
         ) : (
           <NoNotes />
         )}
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,26 @@
-import { ReactChildren } from "@/lib/const/types";
+"use client";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import DashboardSideBar from "./components/sideBar";
+import { ReactChildren } from "@/lib/const/types";
+import useNotes from "@/lib/hooks/useNotes";
+import { useNoteStore } from "@/lib/store/noteStore";
+import { useEffect } from "react";
 import DashboardHeader from "./components/dashboardHeader";
+import DashboardSideBar from "./components/sideBar";
+import { badToast, goodToast } from "@/lib/api/api-status";
 
 const DashboardLayout = ({ children }: ReactChildren) => {
+  const { notesQuery } = useNotes();
+  const { setNotes } = useNoteStore();
+
+  useEffect(() => {
+    if (notesQuery.isSuccess && notesQuery.data?.data) {
+      setNotes(notesQuery?.data?.data);
+    }
+  }, [notesQuery.isSuccess, notesQuery?.data, setNotes]);
+
+  if (notesQuery.isLoading) return goodToast("Loading, Please Wait...");
+  if (notesQuery.isError) return badToast("Error, Please try again.");
+
   return (
     <div className="flex flex-col justify-center items-center w-full h-full relative">
       <SidebarProvider>
