f6620b1e97b712ff15ffe91781c14ca0ac9da22a
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 16:00:43 +0530

    utilized new sidebar toggler.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,14 @@
 import { ReactChildren } from "@/lib/const/types";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import DashboardSideBar from "./components/sideBar";
+import SideBarToggler from "./components/sideBarToggler";
 const HomeLayout = ({ children }: ReactChildren) => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full relative">
       <SidebarProvider>
         <DashboardSideBar />
         <main>
-          <SidebarTrigger />
+          <SideBarToggler />
           {children}
         </main>
       </SidebarProvider>
