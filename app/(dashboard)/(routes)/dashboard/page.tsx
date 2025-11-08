fba50ac4e417f5973e1797bd74f0f3e90a3bd8e5
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 16:57:38 +0530

    simple layout fix.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div className="w-full flex flex-col justify-center items-center"></div>
+  );
 };
 
 export default DashboardPage;
