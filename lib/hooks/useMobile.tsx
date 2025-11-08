f24139b71fd1ebcc7ef61491b7661fbd489eb91b
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 13:31:18 +0530

    usemobile hook for sidebar.

---
+++ lib/hooks/useMobile.tsx
@@ -0,0 +1,23 @@
+// Hook auto-created by shadcnui during sidebar installation.
+
+import * as React from "react";
+
+const MOBILE_BREAKPOINT = 768;
+
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+    undefined
+  );
+
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    };
+    mql.addEventListener("change", onChange);
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    return () => mql.removeEventListener("change", onChange);
+  }, []);
+
+  return !!isMobile;
+}
