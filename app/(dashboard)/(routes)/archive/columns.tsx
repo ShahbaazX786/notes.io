40170b8f2c987eefe1555bd751a368ece4eb122a
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 17:51:27 +0530

    updated select in table.

--- app/(dashboard)/(routes)/archive/columns.tsx
+++ app/(dashboard)/(routes)/archive/columns.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Checkbox } from "@/components/ui/checkbox";
 import { Note } from "@/lib/const/types";
 import { ColumnDef } from "@tanstack/react-table";
 
@@ -7,6 +8,30 @@ import { ColumnDef } from "@tanstack/react-table";
 // I can use a Zod schema here if you want, but first lets make it simple then think about it later.
 
 export const columns: ColumnDef<Note>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value: any) =>
+          table.toggleAllPageRowsSelected(!!value)
+        }
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value: any) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
   {
     accessorKey: "title",
     header: "Title",
