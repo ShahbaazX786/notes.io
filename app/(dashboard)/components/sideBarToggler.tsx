1ebde6a458803de99f54a427ecd753b5236d2fde
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-08 15:59:59 +0530

    Added sidebar toggler button for mobile.

---
+++ app/(dashboard)/components/sideBarToggler.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/components/ui/sidebar";
+import { MdOutlineClose, MdOutlineMenu } from "react-icons/md";
+
+/**
+ * Custom Sidebar Button Component which is used to open and close the sidebar for Mobile.
+ */
+
+const SideBarToggler = () => {
+  const { toggleSidebar, openMobile } = useSidebar();
+
+  return (
+    <div className="flex md:hidden justify-end items-end">
+      <Button onClick={toggleSidebar} variant={"ghost"}>
+        {!openMobile ? (
+          <MdOutlineMenu className="w-6 h-6" />
+        ) : (
+          <MdOutlineClose className="w-6 h-6" />
+        )}
+      </Button>
+    </div>
+  );
+};
+
+export default SideBarToggler;
