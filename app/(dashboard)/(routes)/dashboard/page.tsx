1b07a707cc7809799a32efe965bb36cb1198e486
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-13 16:20:20 +0530

    updated charts in dashboard.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,53 +2,35 @@
 
 import CustomChart from "@/components/customShadcn/customChart";
 import { useNoteStore } from "@/lib/store/noteStore";
-import { cn, filterNotesByStatus } from "@/lib/utils";
+import { filterNotesByStatus, getNotesCreatedByDate } from "@/lib/utils";
 import React from "react";
 
 const DashboardPage = () => {
   const { notes } = useNoteStore();
-  const { completedNotes, pendingNotes, newNotes, noteMetrics } =
-    filterNotesByStatus(notes);
+  const { completedNotes, pendingNotes, newNotes } = filterNotesByStatus(notes);
 
-  const chartData = [
+  const pieData = [
     { name: "Completed", value: completedNotes.length },
     { name: "Pending", value: pendingNotes.length },
     { name: "New", value: newNotes.length },
   ];
 
+  const chartData = getNotesCreatedByDate(notes);
+
   return (
-    <section className="w-full min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-      <section className="w-full h-full flex flex-col justify-start items-center gap-8">
-        <h2 className="text-center font-bold">Status Of Notes</h2>
-        <CustomChart variant="pie" data={chartData} />
-        <div>
-          <p>
-            Total Completion:{" "}
-            <span
-              className={cn(
-                noteMetrics.completionRate > 40
-                  ? "text-green-500"
-                  : "text-red-400"
-              )}
-            >
-              {noteMetrics.completionRate}%
-            </span>
-          </p>
-          <p>
-            Total Pending:{" "}
-            <span
-              className={cn(
-                noteMetrics.pendingRate > 30 ? "text-green-500" : "text-red-400"
-              )}
-            >
-              {noteMetrics.pendingRate}%
-            </span>
-          </p>
-        </div>
+    <section className="w-full min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6 border border-black">
+      <section className="w-full h-fit flex flex-col justify-start items-center gap-8 border rounded-lg hover:shadow-lg py-4">
+        <h2 className="text-center font-bold">Notes by completion status</h2>
+        <CustomChart variant="pie" data={pieData} />
       </section>
 
-      <section className="w-full h-full flex flex-col justify-start items-center gap-8">
+      <section className="w-full h-fit flex flex-col justify-start items-center gap-8 border rounded-lg hover:shadow-lg py-4">
         <h2 className="text-center font-bold">Notes created over time...</h2>
+        <CustomChart variant="line" data={chartData} />
+      </section>
+
+      <section className="w-full h-fit flex flex-col justify-start items-center gap-8 border rounded-lg hover:shadow-lg py-4">
+        <h2 className="text-center font-bold">Notes by popular tags</h2>
         <CustomChart variant="bar" data={chartData} />
       </section>
     </section>
