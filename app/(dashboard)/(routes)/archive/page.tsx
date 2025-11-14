fad1d00dea60bf6488afd0ba93acf7113250d482
shahbaazx786 <shahbaazplatinum@gmail.com>
2025-11-14 12:30:25 +0530

    added page layout.

--- app/(dashboard)/(routes)/archive/page.tsx
+++ app/(dashboard)/(routes)/archive/page.tsx
@@ -7,9 +7,10 @@ const ArchivePage = () => {
   const { notes } = useNoteStore();
 
   return (
-    <div className="w-[80%] mx-auto mt-4">
+    <section className="w-[90%] mx-auto mt-4 border rounded-xl px-2 lg:px-8 py-10">
+      <h1 className="font-semibold text-lg mb-2">Archived Notes</h1>
       <DataTable columns={columns} data={notes} />
-    </div>
+    </section>
   );
 };
 
